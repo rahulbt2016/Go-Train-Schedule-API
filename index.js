809c09c7f971fe9866afa6362c53ff20c578f59b
Rahul Tiwari <rahulbt2016@gmail.com>
2023-04-27 00:25:21 -0400

    added some middleware configs

--- index.js
+++ index.js
@@ -3,10 +3,19 @@ const express = require('express');
 const dotenv = require('dotenv');
 const fs = require('fs/promises');
 const scheduleRoutes = require('./routes/scheduleRoutes');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const helmet = require('helmet');
+
 
 //CONFIGURATIONS
 const app = express();
 dotenv.config();
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(cors());
+app.use(bodyParser.json({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 //DECLARING TRAIN DATA
 var trainData;
@@ -20,6 +29,7 @@ setTrainData = (req, res, next) => {
 //ROUTES
 app.use('/schedule', setTrainData, scheduleRoutes);
 
+//UNDEFINED ROUTES
 app.get('*', (req, res) => {
     res.status(404).json({ message: 'Not Found' });
 });
@@ -29,7 +39,7 @@ const PORT = process.env.PORT || 3000;
 
 //READING JSON FILE
 fs.readFile('./data/go-train-schedule.json')
-  .then(async (data) => {
+  .then((data) => {
     //INITIALIZING TRAIN DATA
     trainData =  JSON.parse(data);
 
--- routes/scheduleRoutes.js
+++ routes/scheduleRoutes.js
@@ -2,7 +2,10 @@ express = require("express");
 const router = express.Router();
 const { getSchedule,  getLineSchedule} = require("../controllers/scheduleController");
 
+//ROUTE THAT RETURNS ALL TRAINS IN THE SCHEDULE
 router.get("/", getSchedule);
+
+//ROUTE THAT RETURNS ALL TRAINS IN THE SCHEDULE FOR A SPECIFIC LINE AND DEPARTURE TIME
 router.get("/:line", getLineSchedule);
 
 module.exports = router;
