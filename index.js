c2fba4672dabee633123e21e5068e57180ab46af
Rahul Tiwari <rahulbt2016@gmail.com>
2023-04-27 13:11:35 -0400

    swagger documentation

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ const scheduleRoutes = require('./routes/scheduleRoutes');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const helmet = require('helmet');
-
+const swaggerUI = require('swagger-ui-express');
+const swaggerJsDoc = require('swagger-jsdoc');
 
 //CONFIGURATIONS
 const app = express();
@@ -17,6 +18,32 @@ app.use(cors());
 app.use(bodyParser.json({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Go Train Schedule API",
+      version: "1.0.0",
+      description: "The GO Train Schedule API is a NodeJS service that provides a simplified train timetable with weekday train times leaving Union Station.",
+      contact: {
+        name: "Rahul Tiwari",
+        email: "rahulbt2016@gmail.com"
+      }
+    },
+    servers: [
+      {
+        url: "https://go-train-schedule-api.azurewebsites.net/"
+      },
+      {
+        url: "http://localhost:3000"
+      }
+    ]
+  },
+  apis: ["./routes/*.js"]
+}
+
+const specs = swaggerJsDoc(options);
+
 //DECLARING TRAIN DATA
 var trainData;
 
@@ -26,7 +53,11 @@ setTrainData = (req, res, next) => {
     next();
 }
 
-//ROUTES
+
+//SWAGGER DOCUMENTATION ROUTE
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(specs));
+
+//SCHEDULE ROUTES
 app.use('/schedule', setTrainData, scheduleRoutes);
 
 //UNDEFINED ROUTES
@@ -44,7 +75,7 @@ fs.readFile('./data/go-train-schedule.json')
     trainData =  JSON.parse(data);
 
     //STARTING SERVER
-    app.listen(PORT, () => {}); 
+    app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`)); 
   })
   .catch((err) => console.log('Cannot read file: ', err.message));
 
--- routes/scheduleRoutes.js
+++ routes/scheduleRoutes.js
@@ -2,10 +2,120 @@ express = require("express");
 const router = express.Router();
 const { getSchedule,  getLineSchedule} = require("../controllers/scheduleController");
 
+/**
+ * @swagger
+ * components: 
+ *  schemas:
+ *     Train: 
+ *       type: object
+ *       properties:
+ *          id:
+ *              type: integer
+ *              description: The train ID 
+ *          line:
+ *              type: string
+ *              description: Line Name
+ *          departure:
+ *              type: integer
+ *              description: Departure Time in 24-Hour Format (Military Time)
+ *          arrival: 
+ *              type: integer
+ *              description: Arrival Time in 24-Hour Format (Military Time)
+ *       example:
+ *          id: 1
+ *          line: "Lakeshore"
+ *          departure: 800
+ *          arrival: 900
+ * 
+ * 
+ */
+
+/**
+ * @swagger
+ * tags:
+ *    name: Train Schedule
+ *    description: The GO Train Schedule API 
+ */
+
 //ROUTE THAT RETURNS ALL TRAINS IN THE SCHEDULE
+/**
+ * @swagger
+ * /schedule:
+ *    get:
+ *     summary: Returns all trains in the schedule
+ *     tags: [Train Schedule]
+ *     responses:
+ *        200:
+ *           description: A list of trains in the schedule
+ *           content:
+ *              application/json:
+ *                 schema:
+ *                   type: array
+ *                   items:
+ *                     $ref: '#/components/schemas/Train'
+ */ 
 router.get("/", getSchedule);
 
 //ROUTE THAT RETURNS ALL TRAINS IN THE SCHEDULE FOR A SPECIFIC LINE AND DEPARTURE TIME
+/**
+ * @swagger
+ * /schedule/{line}:
+ *   get:
+ *     summary: Returns all trains for a specific line in the schedule
+ *     tags: [Train Schedule]
+ *     parameters:
+ *       - in: path
+ *         name: line
+ *         description: The name of the train line to get the schedule for
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: A list of trains in the schedule for the specified line
+ *         content:
+ *           application/json:
+ *             schema:
+ *                type: array
+ *                items:
+ *                $ref: '#/components/schemas/Train'
+ *       404:
+ *         description: Not Found - The specified line does not exist
+ */
+/**
+ * @swagger
+ * /schedule/{line}?departure={departure}:
+ *   get:
+ *     summary: Returns all trains for a specific line, departing at a specific time in the schedule
+ *     tags: [Train Schedule]
+ *     parameters:
+ *       - in: path
+ *         name: line
+ *         description: The name of the train line to get the schedule for
+ *         required: true
+ *         schema:
+ *           type: string
+ *       - in: query
+ *         name: departure
+ *         description: The departure time to get the schedule for (24-Hour or 12-Hour Format)
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: A list of trains in the schedule for the specified line and departure time
+ *         content:
+ *           application/json:
+ *             schema:
+ *                type: array
+ *                items:
+ *                $ref: '#/components/schemas/Train'
+ *       404:
+ *         description: Not Found - The specified line does not exist
+ *       
+ *       400:
+ *        description: Bad Request - The specified departure time is invalid
+ */
 router.get("/:line", getLineSchedule);
 
 module.exports = router;
